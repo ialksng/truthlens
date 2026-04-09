6b2c70615b62f5786ad63db882936932cd8c676c
Alok Singh <ialksng@gmail.com>
2026-04-10 00:54:57 +0530

    Update aiRoutes.js

--- server/routes/aiRoutes.js
+++ server/routes/aiRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { analyzeArticle } from "../controllers/aiController.js";
+import { analyzeArticle, compareArticles } from "../controllers/aiController.js";
 import protect from "../middleware/authMiddleware.js";
 
 const router = express.Router();
